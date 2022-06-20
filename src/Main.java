465994369128e9ea4e735d653efdedf955c4b8a9
user <athimbityana@gmail com>
2022-06-20 12:44:28 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 7, num2=3.5;
+        double addNum=num1 + num2;
+        double moduNum=num1%num2;
+        double multiNum=num1*num2;
+        double divNum=num1/num2;
+
+        System.out.println("Addition " + " " + addNum);
+        System.out.println("Multiplication: " + " " + multiNum);
+        System.out.println("Modulus: " + " " + moduNum);
+
+    }
+}
